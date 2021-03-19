7277cfe11d7d9bad3f57d086ec7bdb8612efc170
Florian Schommertz <johannes@digital-noises.de>
2021-03-19 18:13:46 +0100

    go for it

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'alumni.ikv-aachen.de',
+    title: 'Github.Schommertz.com',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <header>
+      <h1>Florian Schommertz</h1>
+      <div class="avatar">Foto</div>
+    </header>
+    <main>
+      <b-container>
+        <b-row>
+          <b-col>
+            <pre>
+Digital Noises + Florian Schommertz
+Dipl. Sound Engineer, Dipl. Medien-Designer
+</pre
+            >
+            {{ $route.params.id }}
+          </b-col>
+        </b-row>
+      </b-container>
+    </main>
+    <footer class="footer">
+      <div class="text-center"></div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
