469922b6c14e1c248af3a4b79f1e9d52d532839f
Florian Schommertz <johannes@digital-noises.de>
2021-04-06 17:46:01 +0200

    Fun

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,38 @@
 <template>
   <main>
+    <!-- Inspired by: https://codepen.io/natewiley/pen/Ciwyn -->
+    <div id="p-container">
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+      <div class="p"></div>
+    </div>
     <b-container>
       <b-row class="row">
         <b-col cols="9">
@@ -13,10 +46,11 @@
         </b-col>
         <b-col class="mt">
           <p>
-            Consultant for<br />
+            Consultant for:<br />
             Audio Video<br />
-            Online Performance<br />
-            WordPress Themes UX
+            Website-Performance<br />
+            WordPress Themes<br />
+            UX
           </p>
           <p>
             f@schommertz.com // tel: +49 2434.30 83 890<br />
@@ -38,11 +72,49 @@
 export default {}
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+:root {
+  --v: 1.2vmin;
+}
 .row {
   & + .row,
   .mt {
     margin-top: 2em;
+  }
+}
+body {
+  background: #ccc;
+}
+
+.p {
+  position: absolute;
+  border-radius: 50%;
+}
+
+@for $i from 1 through 30 {
+  @keyframes p-animation-#{$i} {
+    100% {
+      transform: translate3d(
+        calc(random(90) * var(--v)),
+        (random(90) * 1.2vh),
+        (random(100) * 1px)
+      );
+    }
+  }
+
+  .p:nth-child(#{$i}) {
+    animation: p-animation-#{$i} 60s infinite;
+    $size: random(5) + 5 + px;
+    opacity: random(100) / 100;
+    height: $size;
+    width: $size;
+    animation-delay: -$i * 0.2s;
+    transform: translate3d(
+      (random(90) * 1vw),
+      (random(90) * 1vh),
+      (random(100) * 1px)
+    );
+    background: hsl(random(360), 70%, 99%);
   }
 }
 </style>
