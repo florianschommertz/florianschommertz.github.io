e4722b6ab38149a0a844f1b1592ecb6cb7a84c6f
Florian Schommertz <johannes@digital-noises.de>
2021-04-06 17:20:38 +0200

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Github.Schommertz.com',
+    title: 'florian.schommertz.com',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
