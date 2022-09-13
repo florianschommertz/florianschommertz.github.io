c16c06da0ebb27b099d696c182200197c099a719
florianschommertz <johannes@digital-noises.de>
2022-09-14 00:28:15 +0200

    merged

--- components/Technologies.vue
+++ components/Technologies.vue
@@ -5,33 +5,35 @@
     <ul>
       <li v-ranking="75">JavaScript</li>
       <li v-ranking="80">Vue.js</li>
-      <li v-ranking="85">jQuery</li>
-      <li v-ranking="90" class="mt-3">WordPress</li>
-      <li v-ranking="45">Drupal</li>
-      <li v-ranking="80" class="mt-3">PHP</li>
-      <li v-ranking="60">MySql</li>
+      <li v-ranking="90">WordPress</li>
+      <li v-ranking="80">
+        PHP
+        <!--  <small> (top 5% on LinkedIn assessment) </small> -->
+      </li>
+      <li v-ranking="60">MySQL / MariaSQL</li>
       <li v-ranking="98">CSS / animation</li>
       <li v-ranking="90">SASS / SCSS</li>
       <li v-ranking="90">HTML</li>
       <li v-ranking="60">SVG</li>
-      <li v-ranking="80" class="mt-3">SEO</li>
+      <li v-ranking="80">SEO</li>
       <li v-ranking="80">Performance</li>
-      <li v-ranking="30" class="mt-3">Threejs</li>
+      <li v-ranking="30">Threejs</li>
       <li v-ranking="75">PWA</li>
       <li v-ranking="75">SPA</li>
-      <li v-ranking="50" class="mt-3">Docker</li>
+      <li v-ranking="50">Docker</li>
       <li v-ranking="65">git</li>
-      <li v-ranking="55">node.js</li>
-      <li v-ranking="70" class="mt-3">RESTful APIs</li>
+
+      <li v-ranking="70">RESTful APIs</li>
       <li v-ranking="70">JSON</li>
       <li v-ranking="35">GraphQl</li>
-      <li class="mt-3">Basic knowledge:</li>
-      <li>node.js, i18n, ssr, vite, vitepress, elementor</li>
+      <li v-ranking="85">jQuery</li>
+      <li>Basic knowledge:</li>
+      <li>Node.js, i18n, SSR, Vite, Vitepress, Elementor, Drupal</li>
     </ul>
     <h3 class="mt-5">design & concept</h3>
     <ul>
-      <li v-ranking="100">Illustrator</li>
-      <li v-ranking="100">Lightroom</li>
+      <li v-ranking="95">Illustrator</li>
+      <li v-ranking="90">Lightroom</li>
       <li v-ranking="85">Photoshop</li>
       <li v-ranking="80">Indesign</li>
       <li v-ranking="60">Adobe XD</li>
