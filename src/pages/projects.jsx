93f7b9280ebaa4a2e1a33ad9274a44d222c55352
florianschommertz <johannes@digital-noises.de>
2022-10-12 08:27:43 +0200

    Portfolio Update

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -100,6 +100,96 @@ const projects2021 = [
   },
 ]
 
+const projects2020 = [
+  {
+    name: 'Gillrath Jobs',
+    description: `A Nuxt JS Jobboard · Optimized for speed`,
+    link: {
+      href: 'https://jobs.gillrath.de/',
+      label: 'jobs.gillrath.de',
+    },
+    logo: logoGillrath,
+    client: 'Gillrath',
+  },
+]
+const projectsBefore = [
+  {
+    name: 'Sequent Medical',
+    description: `An iOS App for neuroradiologist to treat aneurysms. - 2012 build with jQuery-mobile and Cordova`,
+    link: {
+      href: 'https://download.cnet.com/Sequent-Medical/3000-2129_4-76978913.html',
+      label:
+        'link to download.cnet.com, because the app is not available in the app store anymore',
+    },
+    logo: placeHolder,
+    client: 'ZPLUSM · Sequent Medical',
+  },
+  {
+    name: 'John Diva',
+    description: `A band website built with nuxt/vue.js(2) as a headless WordPress, with a plain PHP-Fan Login. The Links up to »Downloads« are Nuxt, the others are outbound or WordPress.`,
+    link: {
+      href: 'https://johndiva.com/',
+      label: 'John Diva (2018)',
+    },
+    logo: placeHolder,
+    client: 'schlicht und ergreifend',
+  },
+  {
+    name: 'Blog for Sigmasoft',
+    description: `A custom WordPress blog theme, built from scratch. The frontpage of the theme had several different layout blocks for the blog articles. At the time of development, CSS grid was bleeding edge. (Integrated in the clients default CMS - All navigation links direct to the current live site)`,
+    link: {
+      href: 'https://portfolio.digital-noises.de/blog.sigmasoft.de/',
+      label: 'link to my portfolio/archive, the blog is not available anymore',
+    },
+    logo: placeHolder,
+    client: 'Sigmasoft',
+  },
+  {
+    name: 'FDI Secrets',
+    description: `PR-Website for a german fashion school.`,
+    link: {
+      href: 'https://portfolio.digital-noises.de/www.fdi-secrets.de/',
+      label: 'link to my portfolio/archive, project has been taken down',
+    },
+    logo: placeHolder,
+    client: 'Fashion Design Institute',
+  },
+
+  {
+    name: 'Kangaroo',
+    description: `A recruiter website based on VueJS and Foundation (CSS) · (The Current Livesite is maintained and altered by client IT.)`,
+    link: {
+      href: 'https://portfolio.digital-noises.de/kangaroo.de/',
+      label: 'Kangaroo',
+    },
+    logo: placeHolder,
+    client: 'Schlicht und ergreifend',
+  },
+  {
+    name: 'Niels Gaury',
+    description: `Portfolio-Website for semi professional photographer Niels and my evaluation project for VueJS. I decided long on the »react/vue« question (after a brief experience with Angular 1). This is the result of my decision at the time that I've not regretted. React however, lost the battle for me, because of Facebooks license at the time.`,
+    link: {
+      href: 'https://www.nielsgaury.de/',
+      label: 'Niels Gaury (2018)',
+    },
+    logo: placeHolder,
+    client: 'Niels Gaury',
+  },
+  {
+    name: 'All others (most outdated/ renewed) - Mostyl WordPress Projects',
+    description: `ironmaiden.de (Live After Death DVD Release) · kylie.de (Kylie Minogue) · johnnylogan.com (Johnny Logan) · diagnostik-hilft.de · zplusm.de ·  sigmasoft.de · bvsa.de · musikkeller.com · auge-wegberg.de · hifi-schluderbacher.de · oliebe.com · darius-mattheisen.de · hoehler-neumann.de · curlsfight.com (Unilevers Beauty Brand Tigi) · parfuemerie-meller.de · costrade.at · buerofueraufmerksamkeit.de · tueschenbroicher-muehle.de · ecoseaker.nz · bronk-objekte.de · notare-svk.de · pettersson-und-findus.de · dassams.de · dieolichis.de · funke-buch.de · malunamondschein.de · norbert-greuel.de · dietributevonpanem.de · oberste-beulmann.de · hospizstiftung-aachen.de · jks-aachen.de · fashion-design-institut.de · praxis-am-rathausplatz.de · fahrschulmanufaktur.de · geheim-garten.de · european-inspire.com (Kyocera Event Website) · oldcountryhousenz.com (Christchurch Hostel) ·
+    simpatec.de · georg-maas.eu · tp-rechtsanwaelte.de · sjs-kanzlei.de · tp-rechtsanwaelte.de · abtei-gerleve.de · syst-beratung.com · schmitz-vornmoor.de · idgmbh.de · habets-aachen.de · landhausbrender.de · startarevolution.de · vitalliin.de · minigolf-wegberg.de · anna-maria-maile.de · homecare-aachen.de · schlemmermarkt-wassenberg.de · savanald.de · your-agency.com · auto-service-twardowski.de · ena-beratung.de · atelieretage-wegberg.de · stormheart.de · marahwoolf.de · animox-buch.de · cjdaugherty.de · oetinger-ohrwuermchen.de · ghetto-bitch.de (Youth Book) · secrets-trilogie.de · buecherstern.de · monday-club.de · zytologie-buettner.de · michels-blumen.de · frohn-gesundheit.de · kgsoiron.de ·  jansen-textil.de · auletta.de · vitalpraxis-roetgen.de · talkcitizen.org · loening.eu · physio-csw.de · ebens.de · ophover-muehle.de · sina-lindberg.de · d.iorama.de · spg-steiner.com · i-love-tigi.de/colourtrip · comnia.de · optik-alberty.de
+    `,
+    link: {
+      href: 'https://www.digital-noises.com/',
+      label: 'Digital Noises',
+    },
+    logo: placeHolder,
+    client:
+      'EMI Music Germany, Squarefashion, schlicht und ergreifend, ZPLUSM  and many more ',
+  },
+]
+
 function LinkIcon(props) {
   return (
     <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
@@ -166,6 +256,44 @@ export default function Projects() {
           className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
         >
           {projects2021.map((project) => (
+            <PortfolioCard
+              key={project.name}
+              name={project.name}
+              logo={project.logo}
+              linkHref={project.link.href}
+              description={project.description}
+              client={project.client}
+              linkLabel={project?.link?.label}
+            ></PortfolioCard>
+          ))}
+        </ul>
+        <h2 className="mt-20 mb-3 text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+          2020
+        </h2>
+        <ul
+          role="list"
+          className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
+        >
+          {projects2020.map((project) => (
+            <PortfolioCard
+              key={project.name}
+              name={project.name}
+              logo={project.logo}
+              linkHref={project.link.href}
+              description={project.description}
+              client={project.client}
+              linkLabel={project?.link?.label}
+            ></PortfolioCard>
+          ))}
+        </ul>
+        <h2 className="mt-20 mb-3 text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+          Old Projects
+        </h2>
+        <ul
+          role="list"
+          className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
+        >
+          {projectsBefore.map((project) => (
             <PortfolioCard
               key={project.name}
               name={project.name}
